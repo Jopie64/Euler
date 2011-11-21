d1ea85f6411e1f1fc5d90865f9de3320269da109
Johan 't Hart <johanthart@gmail.com>
2011-11-21 22:59:11 +0100

    Problem 14 done.

--- Problem14.cpp
+++ Problem14.cpp
@@ -10,7 +10,9 @@ static bool registered = JStd::CmdLine::Register(L"14", problem);
 using namespace std;
 using namespace JStd;
 
-int determineNbStepsWithOutput(int num)
+typedef __int64 calc_int_type;
+
+int determineNbStepsWithOutput(calc_int_type num)
 {
 	cout << num;
 	int steps = 0;
@@ -27,11 +29,46 @@ int determineNbStepsWithOutput(int num)
 	return steps;
 }
 
+int determineNbSteps(calc_int_type num)
+{
+	int steps = 0;
+	while(num != 1)
+	{
+		++steps;
+		if(num & 1)
+			num = 3 * num + 1;
+		else
+			num /= 2;
+	}
+	return steps;
+}
+
+int determineNumberWithMostSteps(int numMax)
+{
+	int numMostSteps = 1;
+	int nbSteps = 0;
+	for(int num = numMax; num > 1; --num)
+	{
+		int currSteps = determineNbSteps(num);
+		if(currSteps > nbSteps)
+		{
+			nbSteps = currSteps;
+			numMostSteps = num;
+			cout << "Number " << numMostSteps << " in " << nbSteps << endl;
+		}
+		if(num % 100 == 0)
+			cout << num << "...\r" << flush;
+	}
+	determineNbStepsWithOutput(numMostSteps);
+	cout << "Done." << endl;
+	return numMostSteps;
+}
+
 void problem(int argc, wchar_t* argv[])
 {
 	if(argc > 2)
 	{
-		determineNbStepsWithOutput(_wtoi(argv[2]));
+		determineNumberWithMostSteps(_wtoi(argv[2]));
 		return;
 	}
 
