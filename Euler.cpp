815ebcbb3dde9c041164e91161d87e6c7b65fa22
Johan 't Hart <johanthart@gmail.com>
2011-11-21 21:24:57 +0100

    Initial

---
+++ Euler.cpp
@@ -0,0 +1,14 @@
+// Euler.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cin.get();
+
+	return 0;
+}
+
