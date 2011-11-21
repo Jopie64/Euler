caf6bd6e6976fdf07b3d54b410fff49edd971442
Johan 't Hart <johanthart@gmail.com>
2011-11-21 22:35:58 +0100

    Problem 14: Can now enter number via console

--- Problem14.cpp
+++ Problem14.cpp
@@ -1,12 +1,48 @@
 #include "stdafx.h"
 
-void problem14(int argc, wchar_t* argv[]);
+namespace problem14
+{
 
-static bool registered = JStd::CmdLine::Register(L"14", problem14);
+void problem(int argc, wchar_t* argv[]);
+
+static bool registered = JStd::CmdLine::Register(L"14", problem);
 
 using namespace std;
 using namespace JStd;
 
-void problem14(int argc, wchar_t* argv[])
+int determineNbStepsWithOutput(int num)
 {
+	cout << num;
+	int steps = 0;
+	while(num != 1)
+	{
+		++steps;
+		if(num & 1)
+			num = 3 * num + 1;
+		else
+			num /= 2;
+		cout << "->" << num;
+	}
+	cout << endl << steps << " steps." << endl;
+	return steps;
+}
+
+void problem(int argc, wchar_t* argv[])
+{
+	if(argc > 2)
+	{
+		determineNbStepsWithOutput(_wtoi(argv[2]));
+		return;
+	}
+
+	int num;
+	do
+	{
+		cout << "Number: ";
+		cin >> num;
+		if(num > 0)
+			determineNbStepsWithOutput(num);
+	}while(num > 0);
+}
+
 }
