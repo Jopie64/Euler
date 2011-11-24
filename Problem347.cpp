2e7f2e981b286b5f61ad1471456ed17a28e4c80b
Johan 't Hart <johanthart@gmail.com>
2011-11-24 20:57:46 +0100

    problem 347: Added code from someone else which is alot quicker then mine...

--- Problem347.cpp
+++ Problem347.cpp
@@ -94,8 +94,40 @@ struct Ctxt
 	}
 };
 
+#define SIEVE 10000000
+
+int D[SIEVE+1];
+int P[SIEVE+1][2];
+bool O[SIEVE+1];
+
+void sieve(){
+	D[0] = D[1] = 1;
+	for(int i=2;i<=SIEVE/2;i++)
+		if(D[i]==0)
+			for(int j=2*i;j<=SIEVE;j+=i){
+				if(D[j]==0)P[j][0]=i;
+				if(D[j]==1)P[j][1]=i;
+				D[j]++;
+			}
+}
+
+int doit(){
+	sieve();
+	long long sum = 0;
+	for(int i=SIEVE;i>0;i--)
+		if(D[i]==2)
+			if(!O[P[i][0]*P[i][1]]){
+				sum+=i;
+				O[P[i][0]*P[i][1]]=true;
+			}
+	printf("%lld\n",sum);
+	return 0;
+}
+
 void problem(int argc, wchar_t* argv[])
 {
+	//doit();
+	//return;
 	if(argc > 2)
 	{
 		int N = _wtoi(argv[2]);
