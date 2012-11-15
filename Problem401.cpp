51f571234b90b185ea054387ae5e64cf69bb9e7e
Johan 't Hart <johanthart@gmail.com>
2012-11-15 21:46:26 +0100

    Problem 401: Added some test code to speed things up. Not working.

--- Problem401.cpp
+++ Problem401.cpp
@@ -13,15 +13,48 @@ static bool registered = JStd::CmdLine::Register(L"401", problem);
 using namespace std;
 using namespace JStd;
 
-__int64 SIGMA2(__int64 n)
+__int64 SIGMA2_Working(__int64 n)
 {
-//	__int64 count = (__int64)sqrt((long double)n);
-	__int64 count = n / 2;
+//	__int64 count1 = (__int64)sqrt((long double)n);
+	__int64 count2 = n / 2;
 	__int64 result = 0;
 	__int64 i=1;
-	for(; i <= count; ++i)
+	for(; i <= count2; ++i)
 	{
 		result += i * i * (n / i);
+	}
+//	for(; i <= count2; ++i)
+//	{
+//		result += i * i * 2;
+//	}
+	for(; i <= n; ++i)
+		result += i * i;
+	return result;
+}
+
+__int64 JSigma(__int64 begin, __int64 end, __int64 factor)
+{
+	return (1 + end - begin) * (end + begin) / 2;
+}
+
+__int64 SIGMA2(__int64 n)
+{
+	__int64 count1 = (__int64)sqrt((long double)n);
+	__int64 count2 = n / 2;
+	__int64 result = 0;
+	__int64 i=1;
+	for(; i <= count2; ++i)
+	{
+		int u1 = i*n;
+		int u2 = i*i*(n/i);
+		if(u1 != u2)
+			cout << "i=" << i << ", n=" << n << " " << u1 << " " << u2 << endl;
+		result += i * n;
+//		result += i * i * (n / i);
+	}
+	for(; i <= count2; ++i)
+	{
+		result += i * i * 2;
 	}
 	for(; i <= n; ++i)
 		result += i * i;
@@ -30,11 +63,13 @@ __int64 SIGMA2(__int64 n)
 
 void problem(CmdLine& cmdLine)
 {
+	JSigma(10, 21, 1);
+
 	cout << "Solving..." << endl;
 
-	__int64 n = 6;
-	for(n=1; n <= 6; ++n)
-		cout << "SIGMA(" << n << ") = " << SIGMA2(n) << endl;
+	__int64 n = 10;
+	//for(n=1; n <= 60; ++n)
+		cout << "SIGMA(" << n << ") = " << SIGMA2(n) << " - Should be: " << SIGMA2_Working(n) << endl;
 }
 
 }
